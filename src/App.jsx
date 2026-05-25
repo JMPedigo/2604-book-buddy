cdb097aef02bf1aaff0db14fa73b25d2f9c5ac84
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-25 15:40:28 -0500

    Creates basic framework for App to use Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { Route, Routes } from "react-router";
+
+/**
+ * Book Buddy is a catalog of books, which users
+ * can reserve or return, once they are logged in.
+ */
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Catalog />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
