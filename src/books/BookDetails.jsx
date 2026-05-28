1cca580d00a5b0731064f805ef4822a8c93786ac
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-28 15:05:05 -0500

    Constructs BookDetails function

--- src/books/BookDetails.jsx
+++ src/books/BookDetails.jsx
@@ -1,3 +1,26 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getBook } from "../api/books";
+
+/** I need a function to call BookDetails and grab id using useParams */
 export default function BookDetails() {
-  return <div>BookDetails</div>;
+  const { id } = useParams();
+  const [book, setBook] = useState(null);
+  const [error] = useState(null);
+
+  useEffect(() => {
+    const syncBook = async () => {
+      const data = await getBook(id);
+      setBook(data);
+    };
+    syncBook();
+  }, [id]);
+  return (
+    <article>
+      <h1>{book.title}</h1>
+      <p>{book.author}</p>
+      <p>{book.description}</p>
+      {error && <p role="alert">{error}</p>}
+    </article>
+  );
 }
