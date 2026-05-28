557a014b6695008645b4758bb6d1a356529e63bd
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-28 14:46:43 -0500

    Wraps books on line 4 within braces to make it an object

--- src/books/CatalogList.jsx
+++ src/books/CatalogList.jsx
@@ -1,12 +1,23 @@
-import CatalogListItem from "./CatalogListItem";
+import { Link } from "react-router";
 
 /** I need a list of books for the CatalogPage */
-export default function CatalogList(books) {
+export default function CatalogList({ books }) {
   return (
     <ul>
       {books.map((book) => (
         <CatalogListItem key={book.id} book={book} />
       ))}
     </ul>
+  );
+}
+
+export function CatalogListItem({ book }) {
+  return (
+    <li>
+      <img src="/books.png" alt="A stack of books." className="navImage" />
+      <Link to={`/books/${book.id}`}>{book.title}</Link>
+      <h2>{book?.author}</h2>
+      <p>{book?.description}</p>
+    </li>
   );
 }
