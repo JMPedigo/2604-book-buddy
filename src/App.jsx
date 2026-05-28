5dc3664ea3f155f8cc618824d91bcc1a286fe073
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-28 18:07:04 -0500

    Adds new routes to login and register

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import CatalogPage from "./books/CatalogPage.jsx";
 import Layout from "./layout/Layout.jsx";
 import Error404 from "./Error404";
 import BookDetails from "./books/BookDetails.jsx";
+import Register from "./auth/Register.jsx";
+import Login from "./auth/Login.jsx";
 
 /**
  * Book Buddy is a catalog of books, which users
@@ -15,9 +17,12 @@ export default function App() {
     <>
       <Routes>
         <Route element={<Layout />}>
+          {/* I need to set my index element to CatalogPage */}
           <Route index element={<CatalogPage />} />
           <Route path="/books" element={<CatalogPage />} />
-          <Route path="/books" element={<BookDetails />} />
+          <Route path="/books/:id" element={<BookDetails />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
           <Route path="*" element={<Error404 />} />
         </Route>
       </Routes>
