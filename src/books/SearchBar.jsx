03138c39f48d929a54ff7d5c48f46b1d9bb8db5c
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-31 14:27:54 -0500

    Cleans up code, renames for clarity

--- src/books/SearchBar.jsx
+++ src/books/SearchBar.jsx
@@ -1,25 +1,28 @@
-import React, { useState } from "react";
+import { useState } from "react";
 /** I need a search bar to search for a specific book title */
 function SearchBar({ onSearch }) {
+  const [error, setError] = useState(null);
   const [searchBook, setSearchBook] = useState("");
-  const handleChange = (event) => {
+  const trySearch = (event) => {
     setSearchBook(event.target.value);
   };
-  const handleSubmit = (event) => {
+  const trySubmit = (event) => {
     event.preventDefault();
+    setError(null);
     onSearch(searchBook);
     setSearchBook("");
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={trySubmit}>
       <label htmlFor="searchBook">Search for a particular book</label>
       <input
         type="text"
         id="searchBook"
         value={searchBook}
-        onChange={handleChange}
+        onChange={trySearch}
       />
       <button type="submit">Search</button>
+      {error && <p role="alert">Book not present in library</p>}
     </form>
   );
 }
